ef1e64401449c0f9d2eb68dc8aa81553a7ebe7b5
chen yu <cyu@infervision.com>
2022-05-20 10:48:02 +0800

    [UPDATE]update cube-react

--- src/examples/cube/cube-react.tsx
+++ src/examples/cube/cube-react.tsx
@@ -4,45 +4,43 @@ import * as THREE from 'three';
 // import requestAnimationFrame from 'raf';
 
 const useCube = () => {
- const [ scene ] = useState(() => new THREE.Scene());
- const [ camera ] = useState(() => new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ));
+  const [ scene ] = useState(() => new THREE.Scene());
+  const [ camera ] = useState(() => new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ));
+  const [ renderer ] = useState(() => new THREE.WebGLRenderer());
 
- const renderer = new THREE.WebGLRenderer();
- renderer.setSize( window.innerWidth, window.innerHeight );
+  useEffect(() => {
+    if(renderer) {
+      // 添加canvas在dom tree里面
+    const element = document.querySelector('.App');
+    if(element) {
+      element.appendChild( renderer.domElement );
+      }
+    }
+  }, [renderer]);
 
- useEffect(() => {
-  if(renderer) {
-    // 添加canvas在dom tree里面
-   const element = document.querySelector('.App');
-   if(element) {
-     element.appendChild( renderer.domElement );
-   }
-  }
- }, [renderer]);
+  useEffect(() => {
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+  
+    camera.position.z = 5;
+  }, [camera, scene]);
 
- useEffect(() => {
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
- 
-  camera.position.z = 5;
- }, [camera, scene]);
-
- useEffect(() => {
-  renderer.render( scene, camera );
-  // cube动画
-   // const checkLoop = () => {
-   //  if(renderer && cube) {
-   //   cube.rotation.x += 0.01;
-   //   cube.rotation.y += 0.01;
-   //   renderer.render( scene, camera );
-   //  }
-   //  requestAnimationFrame(checkLoop);
-   // }
-   // requestAnimationFrame(checkLoop)
- }, [renderer, scene, camera]);
-
+  useEffect(() => {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.render( scene, camera );
+    // cube动画
+    // const checkLoop = () => {
+    //  if(renderer && cube) {
+    //   cube.rotation.x += 0.01;
+    //   cube.rotation.y += 0.01;
+    //   renderer.render( scene, camera );
+    //  }
+    //  requestAnimationFrame(checkLoop);
+    // }
+    // requestAnimationFrame(checkLoop)
+  }, [renderer, scene, camera]);
 };
 
 const Cube = () => {
